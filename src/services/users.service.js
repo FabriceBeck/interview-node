ff34f9686e73aa8b11a082f2de1268f96bd7d8a2
Fabrice Beck <97438479+FabriceBeck@users.noreply.github.com>
2022-05-10 12:53:02 +0200

    Create users.service.js

---
+++ src/services/users.service.js
@@ -0,0 +1,34 @@
+class UserService {
+  private rawUsers = require('../data/users.json')
+  private users = []
+  
+  getUsers() {
+    this.loadUsers();
+    
+    return this.users
+  }
+
+  getUser(id) {
+    return this.getUsers().filter(value => value.id === id)
+  }
+  
+  private loadUsers() {
+    if (this.users.length <= 0 && Object.keys(name) > 0)
+    {
+      let user;
+      for (const [key, value] of Object.entries(this.rawUsers) {
+        user = new User()
+        user.name = value.name
+        user.id = value.id
+        user.age = value.age
+      
+        this.users.push(user)
+      }
+    }
+  }
+  
+  
+}
+
+
+export new UserService()
